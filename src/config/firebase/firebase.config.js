202dcd025a716d95ce0eb25359da840a02f1edfc
Jesus <jesus.gaspar@externos-cl.cencosud.com>
2022-10-01 16:02:01 -0400

    🎨 add standard and test the code

--- src/config/firebase/firebase.config.js
+++ src/config/firebase/firebase.config.js
@@ -1,15 +1,11 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBnyRjw56zsWYKEr0SCf-F7gRHSwiqm3Tw",
-  authDomain: "blogdevg.firebaseapp.com",
-  projectId: "blogdevg",
-  storageBucket: "blogdevg.appspot.com",
-  messagingSenderId: "984463997672",
-  appId: "1:984463997672:web:f514369d12a0aa899da75d",
-  measurementId: "G-9Y93CKK8P7"
-};
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
+}
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export default firebaseConfig
